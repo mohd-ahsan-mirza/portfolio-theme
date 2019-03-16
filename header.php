552028b2ce781d998bfcc54a94a869001f34a06f
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-16 12:27:06 -0400

    Commit 22

--- header.php
+++ header.php
@@ -3,9 +3,10 @@
         <title>Ahsan Mirza</title>
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <script>
-		    var ajaxURL="<?php echo admin_url( 'admin-ajax.php' ); ?>";
-            var emailMeAjaxNonce = "<?php echo wp_create_nonce( 'emailMe-ajax-nonce' ); ?>";
             (function($){
+
+                var ajaxURL="<?php echo admin_url( 'admin-ajax.php' ); ?>";
+            var emailMeAjaxNonce = "<?php echo wp_create_nonce( 'emailMe-ajax-nonce' ); ?>";
     $('.input-message').blur(function(){
 
 		if( !$(this).val().length )
