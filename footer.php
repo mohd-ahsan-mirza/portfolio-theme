2dd63bf5ee16a284e84c9233bd41ad5f89bb7311
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-16 11:37:51 -0400

    Commit 10

--- footer.php
+++ footer.php
@@ -1,4 +1,44 @@
-        <footer>
+        <footer id="footer">
+        <div class="inner">
+
+            <h2>Contact Me</h2>
+<form action="#" method="post" class='contact-form'>
+
+    <div class="field half first">
+        <label for="name">Name</label>
+        <input class='input-name' name="name" id="name" type="text" placeholder="Name">
+        <div class='error name'></div>
+    </div>
+    <div class="field half">
+        <label for="email">Email</label>
+        <input class='input-email' name="email" id="email" type="email" placeholder="Email">
+        <div class='error email'></div>
+    </div>
+    <div class="field">
+        <label for="subject">Subject</label>
+        <input class='input-subject' name="subject" id="subject" type="text" placeholder="Subject">
+        <div class='error subject'></div>
+    </div>
+    <div class="field">
+        <label for="message">Message</label>
+        <textarea class='input-message' name="message" id="message" rows="6" placeholder="Message"></textarea>
+        <div class='error message'></div>
+    </div>
+    <ul class="actions">
+        <li><input class='input-submit' value="Send Message" class="button alt" type="submit"></li>
+    </ul>
+</form>
+
+<ul class="icons">
+    <li><a href='https://ca.linkedin.com/in/muhammad-ahsan-mirza-460b6378' class="icon round fa-linkedin"><span class="label">LinkedIn</span></a></li>
+</ul>
+
+<div class="copyright">
+    &copy; Copyright 2019 Ahsan Mirza
+</div>
+
+</div>
+
         </footer>
     </body>
 </html>
