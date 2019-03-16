ac94a22f4e66ccf60a137515f82c32cb3703cb8a
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-16 14:47:32 -0400

    Commit 33

--- header.php
+++ header.php
@@ -15,5 +15,10 @@
 		<script src="<?php echo site_url('/wp-content/themes/blogtheme/js/util.js'); ?>"></script>
 		<script src="<?php echo site_url('/wp-content/themes/blogtheme/js/main.js'); ?>"></script>
 		<link rel="icon" href="/logo.png">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
+        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 	</head>
 	<body>
