47e6febd577319b3771befc343993d0e5b003a45
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-16 12:23:00 -0400

    Commit 20

--- header.php
+++ header.php
@@ -6,13 +6,13 @@
 		    var ajaxURL="<?php echo admin_url( 'admin-ajax.php' ); ?>";
 		    var emailMeAjaxNonce = "<?php echo wp_create_nonce( 'emailMe-ajax-nonce' ); ?>";
 	    </script>
+        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" />
+        <script src="<?php echo site_url('/wp-content/themes/portfolio-theme/js/main.js'); ?>"></script>
         <script src="<?php echo site_url('/wp-content/themes/portfolio-theme/js/jquery.min.js'); ?>"></script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
         integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" />
-        <script src="<?php echo site_url('/wp-content/themes/portfolio-theme/js/main.js'); ?>"></script>
         <link rel="icon" href="/logo.png">
     </head>
     <body>
