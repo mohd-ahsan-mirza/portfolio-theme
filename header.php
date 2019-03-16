41484cf0c4e25bf04432a52cfaca9c0fb1756d88
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-16 14:25:38 -0400

    Commit 25

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" />
         <script src="<?php echo site_url('/wp-content/themes/portfolio-theme/js/jquery.min.js'); ?>"></script>
         <script src="<?php echo site_url('/wp-content/themes/portfolio-theme/js/main.js'); ?>"></script>
+        <script>console.log('Work')</script>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
         integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
