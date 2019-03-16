9355b80db7a997fea1a85bc7ae8e05921ecdae6e
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-16 10:41:22 -0400

    Commit 6

--- header.php
+++ header.php
@@ -2,12 +2,14 @@
     <head>
         <title>Ahsan Mirza</title>
 	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="<?php echo site_url('/wp-content/themes/portfolio-theme/js/jquery.min.js'); ?>"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" 
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
         <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" />
 	    <script>
 		    var ajaxURL="<?php echo admin_url( 'admin-ajax.php' ); ?>";
 		    var emailMeAjaxNonce = "<?php echo wp_create_nonce( 'emailMe-ajax-nonce' ); ?>";
 	    </script>
-        <script src="<?php echo site_url('/wp-content/themes/portfolio-theme/js/jquery.min.js'); ?>"></script>
         <link rel="icon" href="/logo.png">
     </head>
     <body>
