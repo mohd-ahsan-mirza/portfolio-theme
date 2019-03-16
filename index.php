b04bcf1ef52d12b320a6f53ad12415ee9933c928
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-16 10:50:43 -0400

    Commit 8

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php get_header(); ?>
-<button id='button1' key="1" type='button' className='btn btn-danger'>IT IS WORKING</button>
+<button type='button' className='btn btn-danger'>IT IS WORKING</button>
 <?php get_footer(); ?>
