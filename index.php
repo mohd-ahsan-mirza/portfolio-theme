79bdabe5305e8612535169c2954ec51cf4e81f5e
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-17 19:50:41 -0400

    Commit 80

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php get_header(); ?>
     <section class="wrapper post bg-img" style="background-image: url(../images/cover.jpg);" data-bg="cover.jpg">
+    <div class="inner">
+	</div>
     </section>
 <?php get_footer(); ?>
