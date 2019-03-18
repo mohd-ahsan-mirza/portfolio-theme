78c44901388677586b848bfddb80d844ae4118e7
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-17 23:11:32 -0400

    Commit 142

--- index.php
+++ index.php
@@ -5,13 +5,13 @@
     <section class="card">
         <h1>Ahsan Mirza</h1>
         <p class="title">Software Engineer, Engage People Inc.</p>
-        <p class="bio">3+ years of experience, specializing in designing and supporting 
+        <p class="bio">Specializing in designing and supporting 
             enterprise level software architecture that balances user effectiveness, maintainability, modularity
             and reliability with run-time efficiency.</p>
-        <p class="bio">
-            Programming is much as a hobby as it is a profession and it comes naturally to me. I dedicate a 
-            substantial part of my free time to learning new technologies and doing random programming challenges using 
-            different tools and languages.
+        <p class="info">
+            <ul>
+                <li><span class='info-key'>Experience:<span class='info-value'></span>3+ Years</li>
+            </ul>
         </p>
     </section>
 <?php get_footer(); ?>
