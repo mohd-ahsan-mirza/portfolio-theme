ac113157963125161e7c23c75913ca9adb26676d
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-17 21:12:57 -0400

    Commit 94

--- index.php
+++ index.php
@@ -2,26 +2,9 @@
     <section class="wrapper post bg-img" style="background-image: url(../images/cover.jpg);" data-bg="cover.jpg">
     </section>
     <section>
-    <aside class="profile-card">
-    <h1>
-            John Doe
-          </h1>
-
-    <!-- and role or location -->
-    <h2>
-            Better Visuals
-    </h2>
-
-  <!-- bit of a bio; who are you? -->
-  <div class="profile-bio">
-
-    <p>
-      It takes monumental improvement for us to change how we live our lives. Design is the way we access that improvement.
-    </p>
-
-  </div>
-
-  <!-- some social links to show off -->
-</aside>
-    </section>
+    <section class="card">
+  <h1>John Doe</h1>
+  <p class="title">CEO & Founder, Example</p>
+  <p>Harvard University</p>
+</section>
 <?php get_footer(); ?>
