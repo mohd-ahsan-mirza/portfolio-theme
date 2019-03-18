f18f15e1a4b2d9fc2ee126ad153113423d890700
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-17 22:28:38 -0400

    Commit 127

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
         <p class="bio">3+ years of experience, specializing in designing and supporting 
             enterprise level software architecture that balances user effectiveness, maintainability, modularity
             and reliability with run-time efficiency.</p>
+        <p class="bio">
+            Programming is much as a hobby as it is a profession and it comes naturally to me.I dedicate a substantial part of my free time to learning new technologies and doing random programming challenges using different tools and languages.
+        </p>
     </section>
 <?php get_footer(); ?>
