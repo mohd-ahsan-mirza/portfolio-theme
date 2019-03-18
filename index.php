5808d7dd84faca47cf69b8ca2da2bce98be60521
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-17 21:02:45 -0400

    Commit 89

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php get_header(); ?>
     <section class="wrapper post bg-img" style="background-image: url(../images/cover.jpg);" data-bg="cover.jpg">
     </section>
+    <section>
+    <aside class="profile-card">
+    <h1>
+            John Doe
+          </h1>
+
+    <!-- and role or location -->
+    <h2>
+            Better Visuals
+    </h2>
+
+  <!-- bit of a bio; who are you? -->
+  <div class="profile-bio">
+
+    <p>
+      It takes monumental improvement for us to change how we live our lives. Design is the way we access that improvement.
+    </p>
+
+  </div>
+
+  <!-- some social links to show off -->
+</aside>
+    </section>
 <?php get_footer(); ?>
