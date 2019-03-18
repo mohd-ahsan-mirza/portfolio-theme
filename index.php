c62373e6085361d7e5a23c420d75d1eef8325abf
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-18 19:49:40 -0400

    Commit 245

--- index.php
+++ index.php
@@ -44,6 +44,11 @@
 			    <div class="skillbar-bar" style="background: #F64747;"></div>
 				<div class="skill-bar-percent">1 Year</div>
             </div>
+            <div class="skillbar clearfix " data-percent="40%">
+			    <div class="skillbar-title" style="background: red;"><span>Virtualization</span></div>
+			    <div class="skillbar-bar" style="background: #F64747;"></div>
+				<div class="skill-bar-percent">1 Year</div>
+            </div>
         </div>
         <div class='skills-section2 col-lg-6 col-md-6 col-sm-12'>
             <div class="skillbar clearfix " data-percent="70%">
