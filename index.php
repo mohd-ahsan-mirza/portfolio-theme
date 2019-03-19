f0a830a24afde59fc67d7b4827d2d01b6e5f2a86
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-18 21:25:23 -0400

    Commit 248

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
                 <li><span class='info-key'>Current Job Title:</span><span class='info-value'>Software Engineer</span></li>
                 <li><span class='info-key'>Company:</span><span class='info-value'>Engage People Inc.</span></li>
                 <li><span class='info-key'>Location:</span><span class='info-value'>Toronto</span></li>
-                <li><span class='info-key'>Interests:</span><span class='info-value'>API Development,UI Design, Data Analysis, AI, Solution Architecture</span></li>
+                <li><span class='info-key'>Current Interests:</span><span class='info-value'>UI Design, Data Analysis, AI, Solution Architecture</span></li>
             </ul>
         </div>
     </section>
@@ -25,7 +25,7 @@
         <h1 class='text-center'>SKILLS</h1>
         <div class='skills-section1 col-lg-6 col-md-6 col-sm-12'>
             <div class="skillbar clearfix " data-percent="90%">
-			    <div class="skillbar-title" style="background: red;"><span>PHP</span></div>
+			    <div class="skillbar-title" style="background: red;"><span>API Development</span></div>
 			    <div class="skillbar-bar" style="background: #F64747;"></div>
 				<div class="skill-bar-percent">3 Years</div>
             </div>
@@ -74,7 +74,7 @@
             <div class="skillbar clearfix " data-percent="60%">
 			    <div class="skillbar-title" style="background: red;"><span>CSS</span></div>
 			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">4 months</div>
+				<div class="skill-bar-percent">3 years</div>
             </div>
         </div>
     </section>
