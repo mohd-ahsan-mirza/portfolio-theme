d9a21a33a37ece12773dc1d85a7cf30c5576bde5
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-18 21:28:03 -0400

    Commit 251

--- index.php
+++ index.php
@@ -29,52 +29,12 @@
 			    <div class="skillbar-bar" style="background: #F64747;"></div>
 				<div class="skill-bar-percent">3 Years</div>
             </div>
-            <div class="skillbar clearfix " data-percent="85%">
-			    <div class="skillbar-title" style="background: red;"><span>SQL</span></div>
-			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">3 Years</div>
-            </div>
-            <div class="skillbar clearfix " data-percent="75%">
-			    <div class="skillbar-title" style="background: red;"><span>MongoDB</span></div>
-			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">2 Years</div>
-            </div>
-            <div class="skillbar clearfix " data-percent="45%">
-			    <div class="skillbar-title" style="background: red;"><span>Python</span></div>
-			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">1 Year</div>
-            </div>
-            <div class="skillbar clearfix " data-percent="40%">
-			    <div class="skillbar-title" style="background: red;"><span>Virtualization</span></div>
-			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">1 Year</div>
-            </div>
         </div>
         <div class='skills-section2 col-lg-6 col-md-6 col-sm-12'>
             <div class="skillbar clearfix " data-percent="70%">
-			    <div class="skillbar-title" style="background: red;"><span>JS</span></div>
+			    <div class="skillbar-title" style="background: red;"><span>UI Design</span></div>
 			    <div class="skillbar-bar" style="background: #F64747;"></div>
 				<div class="skill-bar-percent">3 Years</div>
-            </div>
-            <div class="skillbar clearfix " data-percent="90%">
-			    <div class="skillbar-title" style="background: red;"><span>GIT</span></div>
-			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">2 Years</div>
-            </div>
-            <div class="skillbar clearfix " data-percent="65%">
-			    <div class="skillbar-title" style="background: red;"><span>DevOps</span></div>
-			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">2 Years</div>
-            </div>
-            <div class="skillbar clearfix " data-percent="35%">
-			    <div class="skillbar-title" style="background: red;"><span>AWS</span></div>
-			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">4 months</div>
-            </div>
-            <div class="skillbar clearfix " data-percent="60%">
-			    <div class="skillbar-title" style="background: red;"><span>CSS</span></div>
-			    <div class="skillbar-bar" style="background: #F64747;"></div>
-				<div class="skill-bar-percent">3 years</div>
             </div>
         </div>
     </section>
