9e84324218f3a5c4d7aa080081a1a28419967d44
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-20 06:06:42 -0400

    Commit 302

--- index.php
+++ index.php
@@ -81,6 +81,7 @@
         </div>
         <div class="skills-detail-link-section">
             <a href='https://mohd-ahsan-mirza.github.io/digital-resume/'><i class="fab fa-react fa-7x"></i></a>
+            <span font-weight-bold text-center>Click Above for a more in depth look</span>
         </div>
     </section>
 <?php get_footer(); ?>
