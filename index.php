02398056e5b1e54ebe5ed3f3c90820663ddad622
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-24 19:54:40 -0400

    Commit 333

--- index.php
+++ index.php
@@ -78,7 +78,7 @@
             </div>
         </div>
         <div class="skills-detail-link-section">
-            <a href='https://mohd-ahsan-mirza.github.io/digital-resume/'><i class="fab fa-react fa-7x"></i></a>
+            <a class="center-block" href='https://mohd-ahsan-mirza.github.io/digital-resume/'><i class="fab fa-react fa-7x"></i></a>
             <div class="text-center">CLICK ABOVE</div>
         </div>
         <div>
