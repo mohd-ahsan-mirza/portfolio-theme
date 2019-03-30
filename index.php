bc297fc14fdbc9f89e3f23c48d8eb0dd854a66a3
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-29 21:42:15 -0400

    Commit 400

--- index.php
+++ index.php
@@ -122,13 +122,13 @@
         <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="carousel-item active">
-                    <img class="d-block w-100" src="..." alt="First slide">
+                    <img class="d-block w-100" alt="First slide">
                 </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="..." alt="Second slide">
+                    <img class="d-block w-100" alt="Second slide">
                  </div>
                 <div class="carousel-item">
-                    <img class="d-block w-100" src="..." alt="Third slide">
+                    <img class="d-block w-100" alt="Third slide">
                 </div>
             </div>
         </div>
