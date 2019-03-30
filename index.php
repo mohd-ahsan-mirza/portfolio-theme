5c72be279483f3a1a535a68b81525f781cd63e17
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-29 21:59:47 -0400

    Commit 408

--- index.php
+++ index.php
@@ -147,16 +147,16 @@
                     His communication with the client has been fantastic and recognized and I could not be more pleased to 
                     have someone take lead on major projects.
                     </p>
-                </div>      
-            </div>
-            <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
+                </div>
+                <a class="carousel-control-prev" href="#carouselIndicators" role="button" data-slide="prev">
                 <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                 <span class="sr-only">Previous</span>
             </a>
             <a class="carousel-control-next" href="#carouselIndicators" role="button" data-slide="next">
                 <span class="carousel-control-next-icon" aria-hidden="true"></span>
                 <span class="sr-only">Next</span>
-            </a>
+            </a>      
+            </div>
         </div>
     </section>
 <?php get_footer(); ?>
