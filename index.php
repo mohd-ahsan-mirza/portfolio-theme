7617653af071d17773074d90518541e8c8fb147e
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-29 21:37:53 -0400

    Commit 398

--- index.php
+++ index.php
@@ -118,4 +118,19 @@
         <div class="col-lg-4 col-md-6 col-sm-12">
         </div>
     </section>
+    <section class="recommendations">
+        <div class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="..." alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="Second slide">
+                 </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="..." alt="Third slide">
+                </div>
+            </div>
+        </div>
+    </section>
 <?php get_footer(); ?>
