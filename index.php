3aadba081abe03c260092814c1c44622ee7eae0b
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-03-29 22:05:53 -0400

    Commit 413

--- index.php
+++ index.php
@@ -122,7 +122,7 @@
         <div id="carousel-recommendations" class="carousel slide" data-ride="carousel">
             <div class="carousel-inner">
                 <div class="item active">
-                    <p class="text-right">
+                    <p class="text-left">
                     As an Account Director, I rely heavily on our development teams support on one of our largest 
                     nationwide accounts. Thankfully I have Ahsan’s support and expertise at hand. Ahsan is a 
                     dedicated Software Engineer that I have the pleasure of working with for over 1.5 years. 
@@ -136,7 +136,7 @@
                     </p>
                 </div>
                 <div class="item">
-                    <p class="text-right">
+                    <p class="text-left">
                     Ahsan is a developer with a ton of potential and a keen eye for detail. 
                     He has continued to surpass my expectations by tackling all assigned tasks not just from a 
                     technical perspective but also identifying future technical and user problems to deliver quality work. 
