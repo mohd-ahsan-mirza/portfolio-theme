14195acd6495ab9c875f3bf798984f6805575396
Ahsan <mohd.ahsan.mirza@gmail.com>
2019-09-08 13:01:52 -0400

    Commit 3

--- index.php
+++ index.php
@@ -5,9 +5,7 @@
         <div class="bio">
             <span>Specializing in designing and supporting 
             enterprise level software architecture balancing user effectiveness, maintainability, modularity
-            and reliability with run-time efficiency.<br/><br/>Programming is much as a hobby as it is a profession and it 
-            comes naturally to me. I dedicate a substantial part of my free time to learning new technologies and doing 
-            random programming challenges.
+            and reliability with run-time efficiency.<br/><br/>
             </span>
             <ul class="icons">
 				<li>
@@ -38,19 +36,17 @@
             </ul>
         </div>
     </section>
-    <section class='skills'>
+    <!--<<section class='skills'>
         <h1 class='text-center'>SKILLS</h1>
         <div class="skills-section">
             <div class='skills-section1 col-lg-6 col-md-6 col-sm-12'>
                 <div class="skillbar clearfix " data-percent="75%">
                     <div class="skillbar-title" style="background: red;"><span>API Development</span></div>
                     <div class="skillbar-bar" style="background: #F64747;"></div>
-                    <!--<div class="skill-bar-percent">3 Years</div>-->
                 </div>
                 <div class="skillbar clearfix " data-percent="90%">
                     <div class="skillbar-title" style="background: red;"><span>Architecture</span></div>
                     <div class="skillbar-bar" style="background: #F64747;"></div>
-                    <!--<div class="skill-bar-percent">3 Years</div>-->
                 </div>
                 <div class="skillbar clearfix " data-percent="85%">
                     <div class="skillbar-title" style="background: red;"><span>Error Analysis</span></div>
@@ -111,7 +107,7 @@
         <div class="col-lg-4 col-md-6 col-sm-12">
             <a class="img-responsive text-center" href='https://github.com/mohd-ahsan-mirza?tab=repositories'><i class="fab fa-github-square fa-5x"></i></a>
         </div>
-    </section>
+    </section>-->
     <section class="recommendations">
         <h1 class='text-center'>TESTIMONIALS</h1>
         <div id="carousel-recommendations" class="carousel slide" data-ride="carousel">
